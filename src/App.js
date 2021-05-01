f23664a2d5c1e67842862db256ddf3b8a2d5d614
user <kamauri84@gmail.com>
2021-05-01 18:00:24 +1000

    render data to the dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Product from './Product'
 
 function App() {
   return (
     <div className="App">
-
+      <Product />
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react'
+import React, {useState,useEffect } from "react";
 
-export default class Product extends Component {
-    constructor(props){
-        super(props)
+const Product = () =>{
+const [items, setItems] =useState([]);  //Data we fetch from the api
+    const [loading, setLoading] =useState(false); // Know when the item have been loaded from the api endpoint
 
-        this.state ={
-            items: [], //Data we fetch from the api
-            isLoaded: false, // Know when the item have been loaded from the api endpoint 
-        }
-    }
-    render() {
-        return (
-            <div>
+  const getData =()=>{
+    fetch("./productsData.json")
+      .then((res) => res.json()) //Convert to json format
+      .then((json) => {
+        setItems(json);
+        setLoading(true);
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
+    if (!loading) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
                 
-            </div>
-        )
+                {item.price}
+                {item.product_name}
+                {item.description}
+                <img src={item.product_image} alt="product_image"></img>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
     }
+  
 }
+export default Product
